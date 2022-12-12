f6aabe5d15d8a1d4b88265c130eee19bc796c50d
Edward Lo <edward.lo@gcash.co>
2022-12-12 16:13:06 +0800

    feat: add JWT authentication middleware support

---
+++ src/auth/authentication.ts
@@ -0,0 +1,33 @@
+import {Request} from 'express';
+import {verify} from 'jsonwebtoken';
+
+export const secret = 'my@secrEte';
+
+export function expressAuthentication(
+	request: Request,
+	securityName: string,
+	_?: string[]
+): Promise<string | undefined | void> {
+	if (securityName === 'jwt') {
+		const token =
+			request.body.token ||
+			request.query.token ||
+			request.headers['x-access-token'];
+
+		return new Promise((resolve, reject) => {
+			if (!token) {
+				reject(new Error('No token provided'));
+			}
+			verify(token, secret, function (err: any, decoded: any) {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(decoded);
+				}
+			});
+		});
+	}
+	return new Promise((_, reject) => {
+		reject(new Error('securityName not support'));
+	});
+}
