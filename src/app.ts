6f36aed8e87f913423157aced620eddb423b1825
Edward Lo <edward.lo@gcash.co>
2022-12-12 16:08:32 +0800

    feat: init server and app

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, {json, Request as ExRequest, Response as ExResponse, urlencoded} from 'express';
+import {RegisterRoutes} from '../build/routes';
+import swaggerUi from 'swagger-ui-express';
+
+export const app = express();
+
+app.use(
+	urlencoded({
+		extended: true,
+	})
+);
+app.use(json());
+
+app.use('/docs', swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+	return res.send(
+		swaggerUi.generateHTML(await import('../build/swagger.json'))
+	);
+});
+
+RegisterRoutes(app);
